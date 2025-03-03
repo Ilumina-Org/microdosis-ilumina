d052190f16419d7f1e7bb361e4162756fe0f4eb0
Ever Francisco Ponce Lara <ever864@gmail.com>
2025-03-03 14:51:50 -0500

    improve chatbot

--- src/components/ObserverLayout.tsx
+++ src/components/ObserverLayout.tsx
@@ -2,11 +2,11 @@ import React, { use } from 'react';
 // import Products from './Products.astro';
 import NavigationButtons from './ReactComponents/NavigationButtons';
 // import Testimonials from './Testimonials.astro';
-// import Chatbot from './Chatbot.astro';
 import Landing from './ReactPages/Landing';
 import About from './ReactPages/About';
 import Testimonials from './ReactPages/Testimonials';
 import Products from './ReactPages/Products';
+import ChatMedico from './ReactComponents/Chatbot';
 
 
 const ObserverLayout = () => {
@@ -18,12 +18,12 @@ const ObserverLayout = () => {
 
   return (
     <>
-      <NavigationButtons color={'white'}  />
+      <NavigationButtons color={'white'} />
       <Landing horizontalPadding={horizontalPadding} id="inicio" />
       <About horizontalPadding={horizontalPadding} id="about" />
-      <Testimonials horizontalPadding={horizontalPadding} id="testimonios"  />
-      <Products horizontalPadding={horizontalPadding} id="products"  />
-      {/* <Chatbot /> */}
+      <Testimonials horizontalPadding={horizontalPadding} id="testimonios" />
+      <Products horizontalPadding={horizontalPadding} id="products" />
+      <ChatMedico />
     </>
   );
 };
