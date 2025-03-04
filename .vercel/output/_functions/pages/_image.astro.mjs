195b53e5a42229a452f81cb929192da2ec9f44a1
lucas <lucas.videla994@gmail.com>
2025-03-04 04:09:35 -0300

    cleaned some files

--- .vercel/output/_functions/pages/_image.astro.mjs
+++
@@ -1,2 +0,0 @@
-export { a as page } from '../chunks/generic_Dwi2LRUz.mjs';
-export { renderers } from '../renderers.mjs';
--- src/utils/google-sheets.ts
+++ src/utils/google-sheets.ts
@@ -2,27 +2,26 @@ import { google } from "googleapis";
 export const SPREADSHEET_ID = "1g5rj4fIyg0DU9NQuAxN3iBedjPA5aBSgnZZCAHXJr9M";
 export const SHEETS = {
   INVENTORY: "Inventario",
-  ORDERS: "Ordenes",
-  PRODUCTS: "Productos",
+  ORDERS: "Ordenes"
 };
 
 export const getGoogleSheetsClient = async () => {
   const credentials = {
-    type: process.env.GOOGLE_TYPE || "service_account",
-    project_id: process.env.GOOGLE_PROJECT_ID,
-    private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
-    private_key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, "\n"), // Importante: reemplaza \\n por \n
-    client_email: process.env.GOOGLE_CLIENT_EMAIL,
-    client_id: process.env.GOOGLE_CLIENT_ID,
+    type: import.meta.env.GOOGLE_TYPE || "service_account",
+    project_id: import.meta.env.GOOGLE_PROJECT_ID,
+    private_key_id: import.meta.env.GOOGLE_PRIVATE_KEY_ID,
+    private_key: import.meta.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+    client_email: import.meta.env.GOOGLE_CLIENT_EMAIL,
+    client_id: import.meta.env.GOOGLE_CLIENT_ID,
     auth_uri:
-      process.env.GOOGLE_AUTH_URI ||
+      import.meta.env.GOOGLE_AUTH_URI ||
       "https://accounts.google.com/o/oauth2/auth",
     token_uri:
-      process.env.GOOGLE_TOKEN_URI || "https://oauth2.googleapis.com/token",
+      import.meta.env.GOOGLE_TOKEN_URI || "https://oauth2.googleapis.com/token",
     auth_provider_x509_cert_url:
-      process.env.GOOGLE_AUTH_PROVIDER_X509_CERT_URL ||
+      import.meta.env.GOOGLE_AUTH_PROVIDER_X509_CERT_URL ||
       "https://www.googleapis.com/oauth2/v1/certs",
-    client_x509_cert_url: process.env.GOOGLE_CLIENT_X509_CERT_URL,
+    client_x509_cert_url: import.meta.env.GOOGLE_CLIENT_X509_CERT_URL,
   };
 
   const auth = new google.auth.GoogleAuth({
