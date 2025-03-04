153cd25f3d9e6edabbeca39ed1fd0510ce4f8627
lucas <lucas.videla994@gmail.com>
2025-03-04 20:03:34 -0300

    testing mp

--- env.d.ts
+++ env.d.ts
@@ -12,6 +12,12 @@ interface ImportMetaEnv {
   readonly GOOGLE_TOKEN_URI: string;
   readonly GOOGLE_AUTH_PROVIDER_X509_CERT_URL: string;
   readonly GOOGLE_CLIENT_X509_CERT_URL: string;
+  readonly CULQI_SECRET_KEY: string;
+  readonly CULQI_PUBLIC_KEY: string;
+  readonly MP_ACCESS_TOKEN: string;
+  readonly PUBLIC_MERCADOPAGO_PUBLIC_KEY: string;
+  readonly SITE: string;
+  readonly GOOGLE_SHEET_API_KEY: string;
 }
 
 interface ImportMeta {
